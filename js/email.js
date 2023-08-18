11522266ec25ad924eeaea45ea72e82151d435a2
jesusmaringa <vander.vander@gmail.com>
2023-08-18 13:52:37 -0300

    Atualização

--- js/email.js
+++ js/email.js
@@ -1,5 +1,17 @@
 function enviar(){
     let nome = document.getElementById("nomeid").value;
+    let email = document.getElementById("emailid").value;
+    let fone = document.getElementById("foneid").value;
+    let comentario = document.getElementById("comentarioid").value;
+    if(nome == "" || nome == undefined ||
+    email == "" || email == undefined ||
+    fone == "" || fone == undefined ||
+    comentario == "" || comentario == undefined
+    ){
+        alert("Preencha todos os campos.");
+        return;
+    }
+   
     alert("Olá " + nome + ", agradecemos seu contato.");
     location.href = 'index.html';
 }
